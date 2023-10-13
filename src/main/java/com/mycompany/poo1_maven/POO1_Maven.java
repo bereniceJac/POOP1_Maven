1959d2085243eb2986d9da1040afde707ce65a5d
Berenice Jacinto Robledo <146996722+bereniceJac@users.noreply.github.com>
2023-10-12 20:14:18 -0600

    Práctica 1

---
+++ src/main/java/com/mycompany/poo1_maven/POO1_Maven.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poo1_maven;
+
+/**
+ *
+ * @author Jacinto Robledo Valeria Berenice
+ */
+public class POO1_Maven {
+    /**
+     * 
+     * @param args 
+     */
+    public static void main(String[] args) {
+        /**
+         * Mi primer hola mundo en java 
+         */
+        System.out.println("Hola mundo desde POO, grupo 8, desde Maven");
+    }
+}
